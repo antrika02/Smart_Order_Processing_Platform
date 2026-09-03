0f2ad6a6b4076f1d7ba8eecd804927e4369a6236
Antrika Kashyap <antrikakashyap2@gmail.com>
2026-09-03 22:34:43 +0530

    Add validation error handling

--- backend/src/main/java/com/antrika/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/antrika/backend/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.antrika.backend.exception;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.HashMap;
+import java.util.Map;
 import com.antrika.backend.product.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +17,30 @@ public class GlobalExceptionHandler {
     public String handleInvalidCredentials(InvalidCredentialsException ex) {
         return ex.getMessage();
     }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleValidationErrors(
+            MethodArgumentNotValidException ex
+   ) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        return new ValidationErrorResponse(
+         400,
+        "Validation failed",
+                errors
+        );
+   }
 
     @ExceptionHandler(ProductNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
