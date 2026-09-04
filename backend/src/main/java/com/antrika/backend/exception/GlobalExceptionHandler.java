ae52d72b0e77b58675c85eb124c5880c48e475e3
Antrika Kashyap <antrikakashyap2@gmail.com>
2026-09-04 17:42:19 +0530

    Handle insufficient stock errors

--- backend/src/main/java/com/antrika/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/antrika/backend/exception/GlobalExceptionHandler.java
@@ -1,13 +1,15 @@
 package com.antrika.backend.exception;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.antrika.backend.order.exception.InsufficientStockException;
 import com.antrika.backend.product.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,12 +20,11 @@ public class GlobalExceptionHandler {
         return ex.getMessage();
     }
 
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ValidationErrorResponse handleValidationErrors(
             MethodArgumentNotValidException ex
-   ) {
+    ) {
         Map<String, String> errors = new HashMap<>();
 
         ex.getBindingResult()
@@ -36,17 +37,30 @@ public class GlobalExceptionHandler {
                 );
 
         return new ValidationErrorResponse(
-         400,
-        "Validation failed",
+                400,
+                "Validation failed",
                 errors
         );
-   }
+    }
 
     @ExceptionHandler(ProductNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleProductNotFound(ProductNotFoundException ex) {
+    public ApiError handleProductNotFound(
+            ProductNotFoundException ex
+    ) {
         return new ApiError(
                 404,
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(InsufficientStockException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleInsufficientStock(
+            InsufficientStockException ex
+    ) {
+        return new ApiError(
+                409,
                 ex.getMessage()
         );
     }
